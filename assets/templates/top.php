fa78e0ae6f2c301636af5229a5c420090bf734b5
HCairo <hugo.cairoh@gmail.com>
2024-11-05 14:19:24 +0100

    .

--- assets/templates/top.php
+++ assets/templates/top.php
@@ -6,11 +6,16 @@
     <meta name="description" content="HCairo - Personnal development portfolio"> <!-- SEO Description -->
     <meta property="og:title" content="HCairo"> <!-- Social Media Integration -->
     <meta property="og:description" content="Personnal development portfolio">
-    <meta property="og:url" content="http://hcairodev.com/">
-    <base href="http://hcairodev.com/">  <!-- Base URL for relative links / URL de base pour les liens relatifs -->
+    <meta property="og:url" content="https://hcairodev.com/">
+    <base href="https://hcairodev.com/">  <!-- Base URL for relative links / URL de base pour les liens relatifs -->
     <link rel="icon" href="<?= IMG; ?>favicon.ico" type="image/x-icon">  <!-- Favicon icon / Icône favicon -->
     <link rel="shortcut icon" href="<?= IMG; ?>favicon.ico" type="image/x-icon">  <!-- Shortcut icon / Icône raccourci -->
-    <link rel="stylesheet" href="<?= CSS; ?>style.css">  <!-- Main stylesheet / Feuille de style principale -->
+
+    <!-- Preload critical CSS -->
+    <link rel="preload" href="<?= CSS; ?>style.css" as="style">
+    <link rel="stylesheet" href="<?= CSS; ?>style.css">  <!-- Versioning CSS -->
+
+
     <title>HCairo</title>  <!-- Page title / Titre de la page -->
 </head>
 <body>
